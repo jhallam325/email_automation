56c5c7ce7bddf221ce40f7bedc70ee84ba73a7d0
Josh Hallam <83405115+jhallam325@users.noreply.github.com>
2026-04-03 13:30:08 -0700

    gmail doesn't allow 3rd party apps to connect so I switched to yahoo.
    Added a message parameter to the send_email function and get user input for the parameters.

--- email_send.py
+++ email_send.py
@@ -1,6 +1,11 @@
 import smtplib
 
-def send_email(your_email, password, recipient_email):
+your_email = input("Enter your e-mail address: ");
+password = input("Enter your password: ")
+recipient_email = input("Enter the email of the receiver: ");
+message = input("Enter the message: ");
+
+def send_email(your_email, password, recipient_email, message):
     '''
     :param your_email: email address for the gmail account you want to send from
     :type your_email: string
@@ -8,8 +13,10 @@ def send_email(your_email, password, recipient_email):
     :type password: string
     :param recipient_email: recipient's email address
     :type recipient_email: string
+    :param message: the body of the email you want to send
+    :type message: string
     '''
-    host = "smtp.gmail.com"
+    host = "smtp.mail.yahoo.com"
     port = 587
 
     server = smtplib.SMTP(host, port)
@@ -17,8 +24,8 @@ def send_email(your_email, password, recipient_email):
 
     server.login(your_email, password)
 
-    message = "Testing automating email!"
-
     server.sendmail(your_email, recipient_email, message)
 
     server.close()
+
+send_email(your_email, password, recipient_email, message);
